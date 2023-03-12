bc92e6f0b4ad6dbc88fac01774164d3b6329b882
Hermitao <vkriegergranemann@gmail.com>
2023-03-12 13:52:04 -0300

    Add working triangle

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,116 @@
+extern crate gl;
 extern crate sdl2;
 
-extern crate gl;
-use gl::types::*;
+pub mod render_gl;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::video::GLProfile;
-use std::time::Duration;
-use std::ffi::CString;
-use std::ptr;
-use std::str;
-use std::mem;
-use std::os::raw::c_void;
-use std::env;
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
 
-use random::Source;
-
-const vertexShaderSource: &str = r#"
-    #version 430 core
-    layout (location = 0) in vec3 aPos;
-    void main() {
-       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    }
-"#;
-
-pub fn main() {
-    env::set_var("RUST_BACKTRACE", "1");
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
     let gl_attr = video_subsystem.gl_attr();
-    gl_attr.set_context_profile(GLProfile::Core);
-    gl_attr.set_context_version(3, 3);
-    
-    let window = video_subsystem.window("8=D", 800, 600)
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
+    let window = video_subsystem
+        .window("Game", 900, 700)
         .opengl()
+        .resizable()
         .build()
         .unwrap();
 
-    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
-    debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
-    debug_assert_eq!(gl_attr.context_version(), (3, 3));
+    unsafe {
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    use std::ffi::CString;
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
 
-    let mut vertices: [f32; 9] = [
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    let vertices: Vec<f32> = vec![
         -0.5, -0.5, 0.0,
-         0.5, -0.5, 0.0,
-         0.0,  0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0
     ];
-    let (mut vbo, mut vao) = (0, 0);
 
-    let shaderProgram = unsafe {
-                gl::GenBuffers(1, &mut vbo);
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+    }
+
+    unsafe {
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(gl::ARRAY_BUFFER, (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr, &vertices[0] as *const f32 as *const c_void, gl::STATIC_DRAW);
+        gl::BufferData(
+            gl::ARRAY_BUFFER, // target
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
+            gl::STATIC_DRAW, // usage
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
+    }
 
-        
-        let vertexShader = gl::CreateShader(gl::VERTEX_SHADER);
-        let c_str_vert = CString::new(vertexShaderSource.as_bytes()).unwrap();
-        gl::ShaderSource(vertexShader, 1, &c_str_vert.as_ptr(), ptr::null());
-        gl::CompileShader(vertexShader);
-
-        // check for shader compile errors
-        let mut success = gl::FALSE as GLint;
-        let mut infoLog = Vec::with_capacity(512);
-        infoLog.set_len(512 - 1); // subtract 1 to skip the trailing null character
-        gl::GetShaderiv(vertexShader, gl::COMPILE_STATUS, &mut success);
-        if success != gl::TRUE as GLint {
-            gl::GetShaderInfoLog(vertexShader, 512, ptr::null_mut(), infoLog.as_mut_ptr() as *mut GLchar);
-            //println!("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{}", str::from_utf8(&infoLog).unwrap());
-        }
-
-        const fragmentShaderSource: &str = r#"#version 330 core\n
-            out vec4 FragColor;\n
-            void main()\n
-            {\n
-                FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n
-            }"#; 
-
-        let fragmentShader: u32;
-        fragmentShader = gl::CreateShader(gl::FRAGMENT_SHADER);
-        let c_str_frag = CString::new(fragmentShaderSource.as_bytes()).unwrap();
-        gl::ShaderSource(fragmentShader, 1, &c_str_frag.as_ptr(), ptr::null());
-        gl::CompileShader(fragmentShader);
-
-        let shaderProgram = gl::CreateProgram();
-        gl::AttachShader(shaderProgram, vertexShader);
-        gl::AttachShader(shaderProgram, fragmentShader);
-        gl::LinkProgram(shaderProgram);
-        gl::GetProgramiv(shaderProgram, gl::LINK_STATUS, &mut success);
-                if success != gl::TRUE as GLint {
-                    gl::GetProgramInfoLog(shaderProgram, 512, ptr::null_mut(), infoLog.as_mut_ptr() as *mut GLchar);
-                    println!("ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n{}", str::from_utf8(&infoLog).unwrap());
-        }
-        gl::DeleteShader(vertexShader);
-        gl::DeleteShader(fragmentShader);
-
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
-        gl::EnableVertexAttribArray(0);
-
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
         gl::GenVertexArrays(1, &mut vao);
+    }
 
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
-        gl::EnableVertexAttribArray(0);
+    unsafe {
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
 
-        (shaderProgram)
-    };
+        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            0, // index of the generic vertex attribute ("layout (location = 0)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (3 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            std::ptr::null() // offset of the first component
+        );
 
-    'running: loop {
-        unsafe {
-            gl::ClearColor(0.085, 0.09, 0.08, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
 
-            gl::UseProgram(shaderProgram);
-            gl::BindVertexArray(vao);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
-        }
-        window.gl_swap_window();
 
+    'main: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                Event::KeyDown { keycode: Some(Keycode::Q), .. } => {
-                    break 'running
-                },
+                sdl2::event::Event::Quit { .. } => break 'main,
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        shader_program.set_used();
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(
+                gl::TRIANGLES, // mode
+                0, // starting index in the enabled arrays
+                3 // number of indices to be rendered
+            );
+        }
+
+        window.gl_swap_window();
     }
 }
